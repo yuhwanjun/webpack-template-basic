7f09888403a5e6ab532e0ef98471515d20a25caa
yuhwanjun <yuhwanjun0311@gmail.com>
2021-11-25 13:15:36 +0000

    start project

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
+// export
+module.exports = {
+    //파일을 읽어들이기 시작하는 진입점 설정
+    entry: './js/main.js',
+
+    //결과물(번들)을 반환하는 설정
+    output: {
+        // path: path.resolve(__dirname, 'dist'),
+        // filename: 'main.js',
+        clean: true
+    },
+	
+	module: {
+		rules: [
+			{
+				test: /\.s?css$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'postcss-loader',
+					'sass-loader'
+				]
+			},
+			{
+				test: /\.js$/,
+				use: [
+					'babel-loader'
+				]
+			}
+		]
+	},
+	
+	plugins: [
+		new HtmlPlugin({
+			template: './index.html'
+		}),
+		new CopyPlugin({
+			patterns: [
+				{ from: 'static' }
+			]
+		})
+	],
+	devServer: {
+		host: "0.0.0.0",
+		port: 8080,
+		allowedHosts: "all"
+	}
+}
